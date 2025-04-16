6d638c6f5007e2130caacac04d40701146009418
PabloG <gpablo1@yahoo.com.ar>
2025-04-16 23:42:31 +0200

    Enable high touch polling support

--- kona.mk
+++ kona.mk
@@ -376,7 +376,11 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.qti
+    android.hardware.thermal@2.0-service.qti
+
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi
 
 # USB
 PRODUCT_PACKAGES += \
