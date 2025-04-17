a879b060795aab768abe46ecbbd434a8d8036139
PabloG <gpablo1@yahoo.com.ar>
2025-04-17 19:22:45 +0200

    Silence some spammy logging
    HWC and cit sensor logspam is so intense it bumps up logd's idle CPU
    usage up to over 3%. With this change, logs are much cleaner and logd
    idle CPU usage is reduced to under 1%, while still allowing error logs
    to pass through.
    - Do this only in user/debug builds (except eng).
    - Add wifi and cnss too while we're at it, they can get noisy at times.

--- kona.mk
+++ kona.mk
@@ -251,14 +251,21 @@ SPAMMY_LOG_TAGS := \
     MiClstc \
     MiStcImpl \
     SDM \
+    SDM-histogram \
     SRE \
+    WifiHAL \
+    cnss-daemon \
+    libcitsensorservice@2.0-impl \
+    libsensor-displayalgo \
+    libsensor-ssccalapi \
     android.hardware.power-service-qti \
     libsensor-boledalgo \
     libsensor-parseRGB \
     libsensor-qshcalapi \
     sensors \
     vendor.qti.hardware.display.composer-service \
-    vendor.xiaomi.sensor.citsensorservice-service.aidl
+    vendor.xiaomi.sensor.citsensorservice-service.aidl \
+    vendor.xiaomi.sensor.citsensorservice@2.0-service
 
   ifneq ($(TARGET_BUILD_VARIANT),eng)
   PRODUCT_VENDOR_PROPERTIES += \
